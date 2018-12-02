8284b5ad87a07a86fbee339f8508ced1c4a02621
Josep M Sobrepere <josepot@gmail.com>
2018-12-02 02:11:25 +0100

    Day 1 part 2 purely functional

--- src/1/solution2.js
+++ src/1/solution2.js
@@ -1,23 +1,20 @@
-const {head, map, pipe} = require('ramda');
+const {Set} = require('immutable');
+const {head, map, pipe, prop} = require('ramda');
 const {circularLinkedList} = require('../utils/linkedLists');
-
-const findRepeatedFrequency = initialNode => {
-  let node = initialNode;
-  let frequency = 0;
-  const visited = new Set();
-
-  while (!visited.has(frequency)) {
-    visited.add(frequency);
-    frequency += node.value;
-    node = node.next;
-  }
-
-  return frequency;
-};
+const Iterables = require('../utils/iterables');
 
 module.exports = pipe(
   map(parseInt),
   circularLinkedList,
   head,
-  findRepeatedFrequency
+  Iterables.fromLinkedNode,
+  Iterables.whileReducer(
+    ({visited, frequency}) => !visited.has(frequency),
+    ({visited, frequency}, change) => ({
+      visited: visited.add(frequency),
+      frequency: frequency + change,
+    }),
+    {visited: Set(), frequency: 0}
+  ),
+  prop('frequency')
 );
