30a3c36b11e29efd55f4a249285853b0d547b3b8
Josep M Sobrepere <josepot@gmail.com>
2018-12-02 21:18:54 +0100

    Ramdafy Day 2-1

--- src/2/solution1.js
+++ src/2/solution1.js
@@ -1,25 +1,30 @@
-module.exports = lines =>
-  lines
-    .map(line =>
-      line.split('').reduce((acc, char) => {
-        acc[char] = (acc[char] || 0) + 1;
-        return acc;
-      }, {})
-    )
-    .map(lineRes =>
-      Object.values(lineRes)
-        .filter(val => val === 2 || val === 3)
-        .reduce((acc, val) => {
-          acc[val] = (acc[val] || 0) + 1;
-          return acc;
-        }, {})
-    )
-    .reduce(
-      (acc, val) => {
-        acc[0] += val[2] === undefined ? 0 : 1;
-        acc[1] += val[3] === undefined ? 0 : 1;
-        return acc;
-      },
-      [0, 0]
-    )
-    .reduce((x, y) => x * y);
+const {
+  compose: c,
+  converge,
+  identity,
+  either,
+  equals,
+  filter,
+  groupBy,
+  length,
+  map,
+  multiply,
+  pipe,
+  prop,
+  values,
+} = require('ramda');
+
+const getLineGroups = pipe(
+  groupBy(identity),
+  map(length),
+  filter(either(equals(2), equals(3))),
+  values,
+  groupBy(identity)
+);
+
+const getN = id => c(length, filter(prop(id)));
+
+module.exports = pipe(
+  map(getLineGroups),
+  converge(multiply, [getN(2), getN(3)])
+);
