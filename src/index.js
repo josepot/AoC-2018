51bc9a738a94806d6e5712c098bd9f56a0f790a4
Josep M Sobrepere <josepot@gmail.com>
2018-12-03 23:23:24 +0100

    Display execution time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const {compose: c, init, split} = require('ramda');
+const {compose: c, init, split, tap} = require('ramda');
 const {promisify} = require('util');
 const fs = require('fs');
 const {isObservable} = require('rxjs');
 
+let start;
 const log = v => {
+  let end = Date.now();
   if (isObservable(v)) {
     return v.subscribe(console.log, console.error);
   }
   console.log(v);
+  console.log(`Solved in ${end - start}ms`);
 };
 const readFile = promisify(fs.readFile);
 const getLines = c(init, split('\n'));
@@ -15,4 +18,6 @@ const getLines = c(init, split('\n'));
 const [, , day, isSecond] = process.argv;
 const fn = require(`./${day}/solution`)[isSecond ? 1 : 0];
 
-readFile(`./${day}/input`, 'utf-8').then(c(log, fn, getLines));
+readFile(`./${day}/input`, 'utf-8').then(
+  c(log, fn, tap(() => (start = Date.now())), getLines)
+);
