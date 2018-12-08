8512415a75e9bb38107e32affbd0c8449cccbffe
Josep M Sobrepere <josepot@gmail.com>
2018-12-08 07:46:59 +0100

    Day 8 with clean code

--- src/8/solution.js
+++ src/8/solution.js
@@ -1,58 +1,58 @@
-const getSubtree = (source, from) => {
+const {add, compose: c, head, map, pipe, split} = require('ramda');
+
+const getSubtree = (source, from = 0) => {
   const nChilds = source[from];
   const nMetadata = source[from + 1];
+  const dataStart = from + 2;
   const children = [];
+
   if (nChilds === 0) {
     return {
       nChilds,
       nMetadata,
       from,
-      end: from + nMetadata + 1,
-      metadata: source.slice(from + 2, from + 2 + nMetadata),
+      nextStart: dataStart + nMetadata,
+      metadata: source.slice(dataStart, dataStart + nMetadata),
       children: [],
     };
   }
 
-  let next = from + 2;
-  let lastChild;
+  let last = {nextStart: dataStart};
   for (let i = 0; i < nChilds; i++) {
-    lastChild = getSubtree(source, next);
-    next = lastChild.end + 1;
-    children.push(lastChild);
+    last = getSubtree(source, last.nextStart);
+    children.push(last);
   }
 
   return {
     nChilds,
     nMetadata,
     from,
-    end: lastChild.end + nMetadata,
-    metadata: source.slice(lastChild.end + 1, lastChild.end + nMetadata + 1),
+    nextStart: last.nextStart + nMetadata,
+    metadata: source.slice(last.nextStart, last.nextStart + nMetadata),
     children,
   };
 };
 
-const metadataSum = node =>
-  node.metadata.reduce((x, y) => x + y, 0) +
-  node.children.reduce((acc, child) => acc + metadataSum(child), 0);
-
-const solution1 = lines => {
-  const numbers = lines[0].split(' ').map(x => parseInt(x, 10));
-  const tree = getSubtree(numbers, 0);
-  return metadataSum(tree);
-};
+const getMetadataSum = node =>
+  node.metadata.reduce(add) +
+  node.children.reduce((acc, child) => acc + getMetadataSum(child), 0);
 
 const getNodeValue = node =>
   node.children.length === 0
-    ? node.metadata.reduce((x, y) => x + y, 0)
+    ? node.metadata.reduce(add)
     : node.metadata.reduce((acc, idx) => {
         const child = node.children[idx - 1];
         return !child ? acc : acc + getNodeValue(child);
       }, 0);
 
-const solution2 = lines => {
-  const numbers = lines[0].split(' ').map(x => parseInt(x, 10));
-  const tree = getSubtree(numbers, 0);
-  return getNodeValue(tree);
-};
+const getInput = pipe(
+  head,
+  split(' '),
+  map(parseInt)
+);
+const getTree = c(getSubtree, getInput);
+
+const solution1 = c(getMetadataSum, getTree);
+const solution2 = c(getNodeValue, getTree);
 
 module.exports = [solution1, solution2];
