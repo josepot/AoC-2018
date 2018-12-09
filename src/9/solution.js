c818ab90b4dd5f40466098af1a9c28cc9d99e8a1
Josep M Sobrepere <josepot@gmail.com>
2018-12-09 07:27:48 +0100

    Day 9: tidy up

--- src/9/solution.js
+++ src/9/solution.js
@@ -1,31 +1,26 @@
-const R  = require('ramda');
-
-const zeroMarble = {
-  id: 0,
-};
+const zeroMarble = {marble: 0};
 zeroMarble.next = zeroMarble;
 zeroMarble.prev = zeroMarble;
-let current = zeroMarble;
 
+let current = zeroMarble;
 const scores = {};
 
 const scorePoint = (player, marble) => {
   scores[player] = (scores[player] || 0) + marble;
-  for (let i = 0; i < 7; i++) {
-    current = current.prev;
-  }
-  scores[player] += current.id;
+
+  for (let i = 0; i < 7; i++) current = current.prev;
+  scores[player] += current.marble;
+
   current.prev.next = current.next;
   current.next.prev = current.prev;
   current = current.next;
 };
 
 const addMarble = (player, marble) => {
-  if (marble % 23 === 0) {
-    return scorePoint(player, marble);
-  }
+  if (marble % 23 === 0) return scorePoint(player, marble);
+
   const newMarble = {
-    id: marble,
+    marble,
     prev: current.next,
     next: current.next.next,
   };
@@ -34,15 +29,14 @@ const addMarble = (player, marble) => {
   current = newMarble;
 };
 
-const solution1 = () => {
-  const lastMarble = 71920 * 100;
-  const totalPlayers = 403;
-  let currentPlayer = 1;
-  for (let i = 1; i < lastMarble + 1; i++) {
-    addMarble(currentPlayer++ % totalPlayers, i);
-  }
+const play = (lastMarble, totalPlayers) => {
+  for (let i = 1; i < lastMarble + 1; i++) addMarble(i % totalPlayers, i);
   const winner = Object.keys(scores).sort((b, a) => scores[a] - scores[b])[0];
-  return [winner, scores[winner]];
+  return scores[winner];
 };
 
-module.exports = [solution1];
+const input = [71920, 403];
+module.exports = [
+  play.bind(null, ...input),
+  play.bind(null, input[0] * 100, input[1]),
+];
