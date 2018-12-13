ff7f4531baa32194e299483a23899e3a7b1c483c
Josep M Sobrepere <josepot@gmail.com>
2018-12-13 10:27:36 +0100

    Day 13 Clean up code

--- src/13/solution.js
+++ src/13/solution.js
@@ -1,88 +1,66 @@
 const PriorityQueue = require('priorityqueuejs');
 
+const SIZE = 150;
+const grid = new Array(SIZE * SIZE);
+const getPosition = ({x, y}) => grid[y * SIZE + x];
+const setPosition = (x, y, val) => (grid[y * SIZE + x] = val);
+
 const cars = new PriorityQueue((a, b) => {
   if (b.y !== a.y) return b.y - a.y;
   return b.x - a.x;
 });
+const [push, pop, peek, len] = ['enq', 'deq', 'peek', 'size'].map(fnName =>
+  cars[fnName].bind(cars)
+);
 
-const SIZE = 150;
-const grid = new Array(SIZE * SIZE);
-
-const getPosition = ({x, y}) => grid[y * SIZE + x];
-const setPosition = (x, y, val) => (grid[y * SIZE + x] = val);
-
-const moveLeft = car => {
-  if (car.direction.x === 0) {
+const turn = left => {
+  const [a, b] = left ? ['x', 'y'] : ['y', 'x'];
+  return car => {
+    const multiplier = car.direction[a] === 0 ? 1 : -1;
     car.direction = {
-      x: car.direction.y,
-      y: car.direction.x,
+      [a]: car.direction[b],
+      [b]: car.direction[a] * multiplier,
     };
-  } else {
-    car.direction = {
-      x: car.direction.y,
-      y: car.direction.x * -1,
-    };
-  }
-  return car;
+    return car;
+  };
 };
+const [turnLeft, turnRight] = [true, false].map(turn);
 
-const moveRight = car => {
-  if (car.direction.x === 0) {
-    car.direction = {
-      x: car.direction.y * -1,
-      y: car.direction.x,
-    };
-  } else {
-    car.direction = {
-      x: car.direction.y,
-      y: car.direction.x,
-    };
-  }
-  return car;
+const intructions = {
+  '+': car => {
+    const direction = ++car.turns % 3;
+    if (direction === 1) return car;
+    if (direction === 0) return turnLeft(car);
+    return turnRight(car);
+  },
+  '\\': car => (car.direction.x === 0 ? turnLeft : turnRight)(car),
+  '/': car => (car.direction.x === 0 ? turnRight : turnLeft)(car),
 };
 
 const moveCar = car => {
   car.x += car.direction.x;
   car.y += car.direction.y;
+
   const instruction = getPosition(car);
-
-  if (instruction === null) return car;
-  if (instruction === '+') {
-    const direction = ++car.turns % 3;
-    if (direction === 1) return car;
-    if (direction === 0) return moveLeft(car);
-    return moveRight(car);
-  }
-
-  if (instruction === '\\') {
-    if (car.direction.x !== 0) return moveRight(car);
-    return moveLeft(car);
-  }
-  if (instruction === '/') {
-    if (car.direction.x !== 0) return moveLeft(car);
-    return moveRight(car);
-  }
+  return instruction === null ? car : intructions[instruction](car);
 };
 
 const move = () => {
-  const nCars = cars.size();
-  const newCars = new Array(nCars);
-  for (let i = 0; i < nCars; i++) {
-    newCars[i] = moveCar(cars.deq());
-  }
-  for (let i = 0; i < nCars; i++) {
-    cars.enq(newCars[i]);
-  }
+  const nCars = len();
+  const nextCars = new Array(nCars);
+
+  for (let i = 0; i < nCars; i++) nextCars[i] = moveCar(pop());
+  nextCars.forEach(push);
 };
 
 const removeCrashes = () => {
-  const nCars = cars.size();
+  const nCars = len();
   const allCars = new Array(nCars);
   const alive = [];
 
-  allCars[0] = cars.deq();
+  allCars[0] = pop();
   for (let i = 1; i < nCars; i++) {
-    allCars[i] = cars.deq();
+    allCars[i] = pop();
     if (
       allCars[i].x !== allCars[i - 1].x ||
       allCars[i].y !== allCars[i - 1].y
@@ -93,48 +71,41 @@ const removeCrashes = () => {
   if (allCars[0].x !== allCars[1].x || allCars[0].y !== allCars[1].y) {
     alive.push(allCars[0]);
   }
-  alive.forEach(x => cars.enq(x));
+  alive.forEach(push);
 };
 
 const findFirstCrash = () => {
-  const nCars = cars.size();
+  const nCars = len();
   let prevCar = {x: -1, y: -1};
   const newCars = [];
 
   for (let i = 0; i < nCars; i++) {
-    newCars[i] = cars.deq();
+    newCars[i] = pop();
     if (newCars[i].x === prevCar.x && newCars[i].y === prevCar.y) {
       return prevCar;
     }
     prevCar = newCars[i];
   }
-  for (let i = 0; i < nCars; i++) {
-    cars.enq(newCars[i]);
-  }
+  newCars.forEach(push);
   return null;
 };
 
+const directions = {
+  '^': {x: 0, y: -1},
+  v: {x: 0, y: 1},
+  '>': {x: 1, y: 0},
+  '<': {x: -1, y: 0},
+};
 const processInput = lines =>
   lines.forEach((line, y) =>
     line.split('').forEach((val, x) => {
       if (val === '|' || val === '-') return setPosition(x, y, null);
-      if (val === '^') {
-        cars.enq({x, y, turns: -1, direction: {x: 0, y: -1}});
-        return setPosition(x, y, null);
-      }
-      if (val === 'v') {
-        cars.enq({x, y, turns: -1, direction: {x: 0, y: 1}});
-        return setPosition(x, y, null);
-      }
-      if (val === '>') {
-        cars.enq({x, y, turns: -1, direction: {x: 1, y: 0}});
-        return setPosition(x, y, null);
-      }
-      if (val === '<') {
-        cars.enq({x, y, turns: -1, direction: {x: -1, y: 0}});
-        return setPosition(x, y, null);
-      }
-      setPosition(x, y, val);
+
+      const direction = directions[val];
+      if (!direction) return setPosition(x, y, val);
+
+      push({x, y, turns: -1, direction});
+      setPosition(x, y, null);
     })
   );
 
@@ -153,8 +124,8 @@ const solution2 = lines => {
   do {
     move();
     removeCrashes();
-  } while (cars.size() > 1);
-  return [cars.peek().x, cars.peek().y].join(',');
+  } while (len() > 1);
+  return [peek().x, peek().y].join(',');
 };
 
 module.exports = [solution1, solution2];
