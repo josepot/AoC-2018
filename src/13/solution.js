0a5eba753721c7efdc16f029363a48b6a3247a35
Josep M Sobrepere <josepot@gmail.com>
2018-12-14 21:33:59 +0100

    Fix Day 13 code

--- src/13/solution.js
+++ src/13/solution.js
@@ -1,131 +1,116 @@
-const PriorityQueue = require('priorityqueuejs');
+const {
+  doubleCircularLinkedList,
+  circularLinkedList,
+} = require('../utils/linkedLists');
 
-const SIZE = 150;
-const grid = new Array(SIZE * SIZE);
-const getPosition = ({x, y}) => grid[y * SIZE + x];
-const setPosition = (x, y, val) => (grid[y * SIZE + x] = val);
+let N_COLS;
+let N_ROWS;
+let grid;
 
-const cars = new PriorityQueue((a, b) => {
-  if (b.y !== a.y) return b.y - a.y;
-  return b.x - a.x;
-});
-const [push, pop, peek, len] = ['enq', 'deq', 'peek', 'size'].map(fnName =>
-  cars[fnName].bind(cars)
-);
+const getIdx = (x, y) => y * N_COLS + x;
+const getPosition = ({x, y}, from = grid) => from[getIdx(x, y)];
+const setPosition = (x, y, val, to = grid) => (to[getIdx(x, y)] = val);
 
-const turn = left => {
-  const [a, b] = left ? ['x', 'y'] : ['y', 'x'];
-  return car => {
-    const multiplier = car.direction[a] === 0 ? 1 : -1;
-    car.direction = {
-      [a]: car.direction[b],
-      [b]: car.direction[a] * multiplier,
-    };
-    return car;
-  };
+let cars = new Map();
+
+const [UP, RIGHT, DOWN, LEFT] = ['^', '>', 'v', '<'];
+const directions = [UP, RIGHT, DOWN, LEFT];
+const linkedDirections = doubleCircularLinkedList(directions);
+const directionDiffs = {
+  [UP]: {y: -1, x: 0},
+  [DOWN]: {y: 1, x: 0},
+  [LEFT]: {y: 0, x: -1},
+  [RIGHT]: {y: 0, x: 1},
 };
-const [turnLeft, turnRight] = [true, false].map(turn);
 
-const intructions = {
+const [turnLeft, turnRight] = ['prev', 'next'].map(direction => car => {
+  car.direction = car.direction[direction];
+});
+
+const linkedIntersections = circularLinkedList([
+  turnLeft,
+  Function.prototype,
+  turnRight,
+]);
+
+const processInput = lines => {
+  N_ROWS = lines.length;
+  N_COLS = lines[0].length;
+  grid = new Array(N_COLS * N_ROWS);
+  lines.forEach((line, y) =>
+    line.split('').forEach((val, x) => {
+      if (val === '|' || val === '-') return setPosition(x, y, '·');
+      const directionIdx = directions.indexOf(val);
+      if (directionIdx === -1) return setPosition(x, y, val);
+
+      cars.set(getIdx(x, y), {
+        x,
+        y,
+        onIntersection: linkedIntersections[0],
+        direction: linkedDirections[directionIdx],
+      });
+      setPosition(x, y, '·');
+    })
+  );
+};
+
+const instructions = {
   '+': car => {
-    const direction = ++car.turns % 3;
-    if (direction === 1) return car;
-    if (direction === 0) return turnLeft(car);
-    return turnRight(car);
+    car.onIntersection.value(car);
+    car.onIntersection = car.onIntersection.next;
   },
-  '\\': car => (car.direction.x === 0 ? turnLeft : turnRight)(car),
-  '/': car => (car.direction.x === 0 ? turnRight : turnLeft)(car),
+  '\\': car =>
+    ([UP, DOWN].indexOf(car.direction.value) > -1 ? turnLeft : turnRight)(car),
+  '/': car =>
+    ([UP, DOWN].indexOf(car.direction.value) > -1 ? turnRight : turnLeft)(car),
+  '·': Function.prototype,
 };
 
 const moveCar = car => {
-  car.x += car.direction.x;
-  car.y += car.direction.y;
-
-  const instruction = getPosition(car);
-  return instruction === null ? car : intructions[instruction](car);
+  const {x, y} = directionDiffs[car.direction.value];
+  car.x += x;
+  car.y += y;
+  instructions[getPosition(car)](car);
 };
-
-const move = () => {
-  const nCars = len();
-  const nextCars = new Array(nCars);
-
-  for (let i = 0; i < nCars; i++) nextCars[i] = moveCar(pop());
-  nextCars.forEach(push);
-};
-
-const removeCrashes = () => {
-  const nCars = len();
-  const allCars = new Array(nCars);
-  const alive = [];
-
-  allCars[0] = pop();
-  for (let i = 1; i < nCars; i++) {
-    allCars[i] = pop();
-    if (
-      allCars[i].x !== allCars[i - 1].x ||
-      allCars[i].y !== allCars[i - 1].y
-    ) {
-      alive.push(allCars[i]);
-    }
-  }
-  if (allCars[0].x !== allCars[1].x || allCars[0].y !== allCars[1].y) {
-    alive.push(allCars[0]);
-  }
-  alive.forEach(push);
-};
-
-const findFirstCrash = () => {
-  const nCars = len();
-  let prevCar = {x: -1, y: -1};
-  const newCars = [];
-
-  for (let i = 0; i < nCars; i++) {
-    newCars[i] = pop();
-    if (newCars[i].x === prevCar.x && newCars[i].y === prevCar.y) {
-      return prevCar;
-    }
-    prevCar = newCars[i];
-  }
-  newCars.forEach(push);
-  return null;
-};
-
-const directions = {
-  '^': {x: 0, y: -1},
-  v: {x: 0, y: 1},
-  '>': {x: 1, y: 0},
-  '<': {x: -1, y: 0},
-};
-const processInput = lines =>
-  lines.forEach((line, y) =>
-    line.split('').forEach((val, x) => {
-      if (val === '|' || val === '-') return setPosition(x, y, null);
-
-      const direction = directions[val];
-      if (!direction) return setPosition(x, y, val);
-
-      push({x, y, turns: -1, direction});
-      setPosition(x, y, null);
-    })
-  );
 
 const solution1 = lines => {
   processInput(lines);
-  let firstCrash;
   do {
-    move();
-    firstCrash = findFirstCrash();
-  } while (firstCrash === null);
-  return [firstCrash.x, firstCrash.y].join(',');
+    const sortedKeys = [...cars.keys()].sort((a, b) => a - b);
+
+    for (let i = 0; i < sortedKeys.length; i++) {
+      const key = sortedKeys[i];
+      const car = cars.get(key);
+      cars.delete(key);
+      moveCar(car);
+      const id = getIdx(car.x, car.y);
+      if (cars.has(id)) return [car.x, car.y].join(',');
+      cars.set(id, car);
+    }
+  } while (true);
 };
 
 const solution2 = lines => {
   processInput(lines);
   do {
-    move();
-    removeCrashes();
-  } while (len() > 1);
-  return [peek().x, peek().y].join(',');
+    const sortedKeys = [...cars.keys()].sort((a, b) => a - b);
+
+    for (let i = 0; i < sortedKeys.length; i++) {
+      const key = sortedKeys[i];
+      const car = cars.get(key);
+      if (!car) continue;
+      cars.delete(key);
+      moveCar(car);
+      const id = getIdx(car.x, car.y);
+      if (!cars.has(id)) {
+        cars.set(id, car);
+      } else {
+        cars.delete(id);
+      }
+    }
+  } while (cars.size > 1);
+  const [winner] = cars.values();
+  return [winner.x, winner.y].join(',');
 };
 
 module.exports = [solution1, solution2];
