c6230c34894e9218bf71d4df3faa6f8ed9d7bf69
Josep M Sobrepere <josepot@gmail.com>
2018-12-18 07:22:46 +0100

    Day18 cleanup

--- src/18/solution.js
+++ src/18/solution.js
@@ -1,16 +1,12 @@
-const R = require('ramda');
-
 const N_ROWS = 50;
 const N_COLS = 50;
 let grid = new Array(N_ROWS);
 for (let i = 0; i < N_ROWS; i++) grid[i] = new Array(N_COLS);
 
-const patterns = new Map();
-
 const getNeighbours = (x, y) =>
   [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
     .map(([xDiff, yDiff]) => [x + xDiff, y + yDiff])
-    .filter(([x, y]) => x > -1 && y > -1 && x < N_COLS && y < N_COLS)
+    .filter(([x, y]) => x > -1 && y > -1 && x < N_COLS && y < N_ROWS)
     .map(([x, y]) => grid[y][x]);
 
 const options = {
@@ -25,32 +21,18 @@ const options = {
       : '.',
 };
 
-const getGridId = grid => grid.reduce((acc, line) => acc + line.join(''), '');
-
-const turn = () => {
-  grid = grid.map((line, y) =>
-    line.map((val, x) => {
-      const nei = getNeighbours(x, y);
-      return options[val](nei);
-    })
-  );
-};
-
-const findCycle = () => {
-  for (let i = 0; i < 1000000000; i++) {
-    turn();
-    const id = getGridId(grid);
-    if (patterns.has(id)) return [patterns.get(id), i];
-    patterns.set(id, i);
-  }
-};
-
 const parseInput = lines =>
   lines.forEach((line, y) =>
     line.split('').forEach((c, x) => {
       grid[y][x] = c;
     })
   );
+
+const turn = () => {
+  grid = grid.map((line, y) =>
+    line.map((val, x) => options[val](getNeighbours(x, y)))
+  );
+};
 
 const countType = type => {
   return grid.reduce(
@@ -64,6 +46,18 @@ const solution1 = lines => {
   parseInput(lines);
   for (let i = 0; i < 10; i++) turn(i);
   return countType('|') * countType('#');
+};
+
+const getGridId = grid => grid.reduce((acc, line) => acc + line.join(''), '');
+
+const findCycle = () => {
+  const patterns = new Map();
+  for (let i = 0; i < Infinity; i++) {
+    turn();
+    const id = getGridId(grid);
+    if (patterns.has(id)) return [patterns.get(id), i];
+    patterns.set(id, i);
+  }
 };
 
 const solution2 = lines => {
