bb3a44adb8be553aa5ec83853e6fca6d58f932ce
Josep M Sobrepere Profitós <josepot@gmail.com>
2018-12-21 16:24:15 +0100

    Day 20 alternative solution

--- src/20/solution.js
+++ src/20/solution.js
@@ -1,19 +1,23 @@
+const {compose: c} = require('ramda');
 const Stack = require('../utils/stack');
 
-const distances = new Map();
+const openDoors = new Map();
+const distances = new Map([['0,0', 0]]);
 const groups = new Stack();
-let position = {x: 0, y: 0, distance: 0};
+let position = {x: 0, y: 0};
+
+const addDoor = ({x, y}, xDiff, yDiff) => {
+  const positionKey = `${x},${y}`;
+  const doorKey = `${xDiff},${yDiff}`;
+  if (!openDoors.has(positionKey)) openDoors.set(positionKey, new Set());
+  openDoors.get(positionKey).add(doorKey);
+};
 
 const advance = (xDiff, yDiff) => {
+  addDoor(position, xDiff, yDiff);
   position.x += xDiff;
   position.y += yDiff;
-  position.distance++;
-
-  const key = `${position.x}-${position.y}`;
-  const currentDistance = distances.get(key) || Infinity;
-  if (position.distance < currentDistance) {
-    distances.set(key, position.distance);
-  } else position.distance = currentDistance;
+  addDoor(position, xDiff * -1, yDiff * -1);
 };
 
 const options = {
@@ -26,13 +30,37 @@ const options = {
   ')': () => (position = groups.pop()),
 };
 
-const solve = ([rawInput]) =>
+const buildGrid = ([rawInput]) =>
   rawInput
     .slice(1, -1)
     .split('')
     .forEach(c => options[c]());
 
+const setDistances = () => {
+  let unexplored = [`0,0`];
+  let distance = 1;
+  let nextUnexplored = [];
+  do {
+    unexplored.forEach(positionId => {
+      const available = [...openDoors.get(positionId)]
+        .map(door => {
+          const [xDiff, yDiff] = door.split(',').map(x => parseInt(x, 10));
+          const [x, y] = positionId.split(',').map(x => parseInt(x, 10));
+          return `${x + xDiff},${y + yDiff}`;
+        })
+        .filter(id => !distances.has(id));
+      nextUnexplored.push.apply(nextUnexplored, available);
+      available.forEach(id => distances.set(id, distance));
+    });
+    unexplored = nextUnexplored;
+    nextUnexplored = [];
+    distance++;
+  } while (unexplored.length > 0);
+};
+
+const solve = c(setDistances, buildGrid);
+
 const solution1 = () => Math.max.apply(null, [...distances.values()]);
 const solution2 = () => [...distances.values()].filter(x => x >= 1000).length;
 
-module.exports = [solution1, solution2].map(fn => x => fn(solve(x)));
+module.exports = [solution1, solution2].map(fn => c(fn, solve));
